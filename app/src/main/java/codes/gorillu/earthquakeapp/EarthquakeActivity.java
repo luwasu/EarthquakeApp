cec4071227f976013abe9ed74043b700f5c436bf
LuSantanaHolmes <luis.santanaholmes@gmail.com>
2020-05-26 23:48:13 +0200

    add JSON mock data

--- app/src/main/java/codes/gorillu/earthquakeapp/EarthquakeActivity.java
+++ app/src/main/java/codes/gorillu/earthquakeapp/EarthquakeActivity.java
@@ -15,23 +15,18 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        // Get the list of earthquakes form {@link QueryUtils}
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake("6.1", "London", "July 20, 2015"));
-        earthquakes.add(new Earthquake("3.9", "Tokyo", "Nov, 2014"));
-        earthquakes.add(new Earthquake("5.4", "Mexico City", "May 3, 2014"));
-        earthquakes.add(new Earthquake("2.8", "Moscow", "Jan 31, 2013"));
-        earthquakes.add(new Earthquake("4.9", "Rio de Janeiro", "Aug 19, 2012"));
-        earthquakes.add(new Earthquake("1.6", "Paris", "Oct 30, 2011"));
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeAdapter = findViewById(R.id.list_view);
 
+        // Create a new adapter that takes the list of earthquakes as input
+        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
 
-        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, earthquakes);
-
-
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(earthquakeAdapter);
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeAdapter.setAdapter(adapter);
     }
 }
 
