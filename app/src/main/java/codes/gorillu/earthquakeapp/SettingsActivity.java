b25e3bb4edef68c877d13390b9b283f7b6440c45
luwasu <luis.santanaholmes@gmail.com>
2020-07-02 15:19:15 +0200

    add "order by" menu

--- app/src/main/java/codes/gorillu/earthquakeapp/SettingsActivity.java
+++ app/src/main/java/codes/gorillu/earthquakeapp/SettingsActivity.java
@@ -1,11 +1,13 @@
 package codes.gorillu.earthquakeapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -16,30 +18,45 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
     }
-
-//    public static class EarthquakePreferenceFragment extends PreferenceFragmentCompat {
-//
-//        @Override
-//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-//
-//        }
-//
-//        @Override
-//        public void onCreate(Bundle savedInstanceState) {
-//            super.onCreate(savedInstanceState);
-//            addPreferencesFromResource(R.xml.settings_main);
-//        }
-//    }
-
 }
 
 
